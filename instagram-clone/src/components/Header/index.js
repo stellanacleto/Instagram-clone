8eb4f40ad59416e88cf3353c7d1d02208f4340cf
stellanacleto <stellanacleto@gmail.com>
2024-04-05 12:23:35 -0300

    first commit

---
+++ instagram-clone/src/components/Header/index.js
@@ -0,0 +1,11 @@
+export function Header(){
+    return (
+        <header className= "header">
+
+            <div className="container">
+                <img className="logo" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
+            </div>
+
+        </header>
+    )
+}
