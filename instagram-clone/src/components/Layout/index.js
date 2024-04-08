fc1d71837713d18dcbe84e919965c5248f1b8a4e
Malu Vasconcelos <140660848+lvathayde@users.noreply.github.com>
2024-04-08 18:36:58 -0300

    Story

---
+++ instagram-clone/src/components/Layout/index.js
@@ -0,0 +1,30 @@
+import './style.css'
+
+import { Story } from '../Story'
+
+export function Layout() {
+
+    return (
+        <>
+
+            <div className='MainGrid'>
+
+                <div className='first-column' style={{gridArea: "firstColumn"}}>
+                    <div className="box">
+                        <Story />
+                    </div>
+
+                    <div className="box">
+                        post
+                    </div>
+                </div>
+
+                <div className="suggestionBox" style={{gridArea: "secondColumn"}}>
+                    sugestão
+                </div>
+                                
+            </div>
+            
+        </>
+    )
+}
